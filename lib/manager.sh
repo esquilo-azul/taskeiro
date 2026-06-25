a18acc2ee1257302c6d0a55fe3b89c2afb95fce0
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-06-25 16:12:13 -0300

    _task_run(): move to "lib/_task_run.sh".

--- lib/manager.sh
+++ lib/manager.sh
@@ -45,27 +45,6 @@ function taskeiro_run() {
   printf "$TASKEIRO_TASKS" | while read TASK; do _task_run "$TASK" ; done
 }
 
-function _task_run() {
-  _debug "START $1"
-  if _task_checked "$1" ; then
-    _debug "END $1 (ALREADY CHECKED: $TASKEIRO_CHECKED_TASKS)"
-    return
-  fi
-  _task_check "$1"
-  for dep in $(taskeiro_task_dependencies "$1"); do
-    _debug "DEPENDENCY $1 -> $dep"
-    _task_run "$dep"
-  done
-  _taskeiro_path_callback '_before_task.sh'
-  if ! _task_pass "$1" 1 ; then
-    _call_task_function "$1" task_fix
-    if ! _task_pass "$1" 0 ; then
-      fatal_error "Task \"$1\" failed to pass"
-    fi
-  fi
-  _debug "END $1"
-}
-
 function _taskeiro_path_callback() {
   SUBPATH="$1"
   local IFS=:
