1565ba5f5c200eeed2cc98680282c64dee749a29
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-06-25 15:32:19 -0300

    _task_checked(): move to "lib/_task_checked.sh".

--- lib/manager.sh
+++ lib/manager.sh
@@ -66,11 +66,6 @@ function _task_run() {
   _debug "END $1"
 }
 
-function _task_checked() {
-  _validate_task_name "$1"
-  echo "$TASKEIRO_CHECKED_TASKS" | grep "|$1|" > /dev/null
-}
-
 
 function _task_pass() {
   if _call_task_function "$1" task_condition ; then
