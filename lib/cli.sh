35f51bbd87c3bce7d1e3ea0db4259fa942759f16
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-06-25 16:04:17 -0300

    taskeiro_read_args(): move to "lib/taskeiro_read_args.sh".

--- lib/cli.sh
+++ lib/cli.sh
@@ -1,23 +1,3 @@
-function taskeiro_read_args() {
-  while [[ $# > 0 ]] ; do
-    key="$1"
-    case $key in
-      -h|--help)
-      taskeiro_help
-      exit 0
-      ;;
-      -p|--path)
-      TASKEIRO_PATH="$2"
-      shift
-      ;;
-      *)
-      TASKEIRO_TASKS="$TASKEIRO_TASKS$1\n"
-      ;;
-    esac
-    shift
-  done
-}
-
 function taskeiro_start_banner() {
   infov 'Path' "$TASKEIRO_PATH"
   local tasks=$(printf "$TASKEIRO_TASKS" | xargs -ILINE printf "LINE ")
