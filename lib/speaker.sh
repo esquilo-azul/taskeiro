a06219546588a6a691d2de429272ac44f7088ff3
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-06-25 14:17:26 -0300

    _debug(): move to "lib/_debug.sh".

--- lib/speaker.sh
+++ lib/speaker.sh
@@ -3,9 +3,3 @@ export FG_LGREEN='\e[92m'
 export FG_LRED='\e[91m'
 export FG_LYELLOW='\e[93m'
 export FG_LBLUE='\e[94m'
-
-function _debug() {
-  if [ -n "$TASKEIRO_DEBUG" ]; then
-    outerr "${FG_LBLUE}" "$@" "${NC}\n"
-  fi
-}
