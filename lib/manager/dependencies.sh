e93fa99ec80efc9e0922d9694ebea5ee2ae21cd5
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-06-25 13:10:48 -0300

    _extra_dependencies_add(): move to "lib/_extra_dependencies_add.sh".

--- lib/manager/dependencies.sh
+++ lib/manager/dependencies.sh
@@ -22,19 +22,6 @@ function taskeiro_add_dependency() {
   done
 }
 
-function _extra_dependencies_add() {
-  local TARGET="$1"
-  local DEPENDENCY="$2"
-
-  local VALUE="$(hash_get 'EXTRA_DEPENDENCIES' "$1")"
-  if [[ -n "$VALUE" ]]; then
-    VALUE="$VALUE "
-  fi
-  VALUE="${VALUE}${DEPENDENCY}"
-
-  hash_put 'EXTRA_DEPENDENCIES' "$TARGET" "$VALUE"
-}
-
 function _extra_dependencies_get() {
   hash_get 'EXTRA_DEPENDENCIES' "$1"
 }
