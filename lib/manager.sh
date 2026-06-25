8d0475b7dd3c47464e3e9442348048db84e12668
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-06-25 16:10:52 -0300

    _taskeiro_path_callback(): move to "lib/_taskeiro_path_callback.sh".

--- lib/manager.sh
+++ lib/manager.sh
@@ -15,22 +15,6 @@ function taskeiro_task_path() {
   return 1
 }
 
-function _taskeiro_path_callback() {
-  SUBPATH="$1"
-  _debug "START _taskeiro_path_callback()" "$@"
-  local IFS=:
-  for p in $TASKEIRO_PATH; do
-    BEFORE_RUN_PATH="${p}/${SUBPATH}"
-    if [ -f "$BEFORE_RUN_PATH" ]; then
-      _debug "Callback ${BEFORE_RUN_PATH}: found"
-      source "$BEFORE_RUN_PATH"
-    else
-      _debug "Callback ${BEFORE_RUN_PATH}: not found"
-    fi
-  done
-  _debug "END _taskeiro_path_callback()" "$@"
-}
-
 function _validate_task_name() {
   if ! _task_valid_name "$1"; then
     fatal_error "Invalid task name: \"$1\""
