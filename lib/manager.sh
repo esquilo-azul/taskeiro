d67c0ee264c53cd07405e6ebd5c29fb595e0f926
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-06-25 14:21:37 -0300

    _call_task_function(): move to "lib/_call_task_function.sh".

--- lib/manager.sh
+++ lib/manager.sh
@@ -113,23 +113,6 @@ function _task_message_condition {
   infov "$1" "$m"
 }
 
-function _call_task_function {
-  local task=$1
-  local script=$(taskeiro_task_path "$task")
-  local function_name=$2
-  local required='0'
-  if [ $# -ge 3 ]; then
-    required=$3
-  fi
-  unset -f $function_name
-  source "$script"
-  if _function_exists "$function_name"; then
-    "$function_name"
-  elif [ "$required" == '0' ]; then
-    fatal_error "Function \"$function_name\" not found for task \"$task\""
-  fi
-}
-
 function _function_exists() {
   type "$1" 2> /dev/null > /dev/null
 }
