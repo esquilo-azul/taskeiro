43edbdf8a1105d0839cf00a3abfb24aef216ad84
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-06-25 15:36:26 -0300

    _task_pass(): move to "lib/_task_pass.sh".

--- lib/manager.sh
+++ lib/manager.sh
@@ -66,17 +66,6 @@ function _task_run() {
   _debug "END $1"
 }
 
-
-function _task_pass() {
-  if _call_task_function "$1" task_condition ; then
-    RESULT=0
-  else
-    RESULT=1
-  fi
-  _task_message_condition "$1" "$RESULT" "$2"
-  return $RESULT
-}
-
 function _taskeiro_path_callback() {
   SUBPATH="$1"
   local IFS=:
@@ -87,4 +76,3 @@ function _taskeiro_path_callback() {
     fi
   done
 }
-
