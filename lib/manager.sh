ca6ac147a6417b67fee079682f3d21ea58792876
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-06-25 15:33:18 -0300

    _task_message_condition(): move to "lib/_task_message_condition.sh".

--- lib/manager.sh
+++ lib/manager.sh
@@ -88,18 +88,3 @@ function _taskeiro_path_callback() {
   done
 }
 
-function _task_message_condition {
-  local result=$2
-  local after=$3
-  local m=''
-  if [ "$2" == '0' ]; then
-    m=$m"${FG_LGREEN}ok${NC}"
-  else
-    m=$m"${FG_LRED}not ok${NC}"
-  fi
-  if [ "$3" == '0' ]; then
-    m=$m' (AFTER FIX)'
-  fi
-  m=$m" $FG_LYELLOW[$(taskeiro_task_dependencies "$1")]$NC"
-  infov "$1" "$m"
-}
