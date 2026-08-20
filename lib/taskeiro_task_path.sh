bcd6b8b3d7419800df03505128f71d1d1e0fd061
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-08-20 15:24:59 -0300

    taskeiro_task_path(): refact with pathvar_find_file().

--- lib/taskeiro_task_path.sh
+++ lib/taskeiro_task_path.sh
@@ -1,12 +1,3 @@
 function taskeiro_task_path() {
-  local IFS=:
-  for p in $TASKEIRO_PATH; do
-    TARGET_PATH="$p/$1.sh"
-    if [ -f "$TARGET_PATH" ]; then
-      echo "$TARGET_PATH"
-      return 0
-    fi
-  done
-  >&2 echo "Task file not found for name \"$1\""
-  return 1
+  pathvar_find_file 'TASKEIRO_PATH' "$1.sh"
 }
