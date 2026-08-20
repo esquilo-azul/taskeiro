88d09af7d086559ecdf23a94b81216e44e4503e2
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-08-20 15:35:36 -0300

    _taskeiro_path_callback(): refact with pathvar_find_files().

--- lib/_taskeiro_path_callback.sh
+++ lib/_taskeiro_path_callback.sh
@@ -1,15 +1,11 @@
 function _taskeiro_path_callback() {
   SUBPATH="$1"
   _debug "START _taskeiro_path_callback()" "$@"
-  local IFS=:
-  for p in $TASKEIRO_PATH; do
-    BEFORE_RUN_PATH="${p}/${SUBPATH}"
-    if [ -f "$BEFORE_RUN_PATH" ]; then
-      _debug "Callback ${BEFORE_RUN_PATH}: found"
-      source "$BEFORE_RUN_PATH"
-    else
-      _debug "Callback ${BEFORE_RUN_PATH}: not found"
-    fi
-  done
+
+  while read -r BEFORE_RUN_PATH; do
+    _debug "Callback ${BEFORE_RUN_PATH}: found"
+    source "$BEFORE_RUN_PATH"
+  done < <(pathvar_find_files 'TASKEIRO_PATH' "$SUBPATH" )
+
   _debug "END _taskeiro_path_callback()" "$@"
 }
